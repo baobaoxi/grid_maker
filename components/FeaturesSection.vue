7a7b8e65dcdf6f7c395a97ea3bd807bc8133d33f
Manlong Feng <xinyangxi0520@163.com>
2026-05-27 11:08:29 +0800

    feat: 修改一部分内容

--- components/FeaturesSection.vue
+++ components/FeaturesSection.vue
@@ -45,11 +45,6 @@
               <img :src="item.imageUrl" alt="Grid Maker for people to draw artist" />
             </div>
             <div class="why-content">
-              <div class="why-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <path :d="item.iconPath" />
-                </svg>
-              </div>
               <h4>{{ item.title }}</h4>
               <p>{{ item.description }}</p>
             </div>
