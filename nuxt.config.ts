f8c0b82a79b8b7923812310a62b5a2e0d46df7cf
Manlong Feng <xinyangxi0520@163.com>
2026-06-01 20:03:01 +0800

    feat: description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'The ultimate Grid Maker for beginners.' }
+        { name: 'description', content: 'The ultimate Grid Maker for beginners. Create professional artwork with zero drawing skills. Completely free, no sign-up needed. Upload your photo now!' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
