4b3562f662a265b3b5a6c71beafee3a0aa4bb758
Manlong Feng <xinyangxi0520@163.com>
2026-06-24 11:38:41 +0800

    feat: bug

--- .output/server/chunks/build/styles.mjs
+++ .output/server/chunks/build/styles.mjs
@@ -1,29 +1,29 @@
 const interopDefault = r => r.default || r || [];
 const styles = {
   "node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.D-7ZkAoz.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.CiJjK6WX.mjs').then(interopDefault),
-  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.ulHNpcF1.mjs').then(interopDefault),
   "pages/blog/index.vue": () => import('./index-styles.BEAdg63l.mjs').then(interopDefault),
   "pages/admin/blogs.vue": () => import('./blogs-styles.kouJTMoQ.mjs').then(interopDefault),
   "pages/blog/[slug].vue": () => import('./_slug_-styles.hbdvuABm.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.ulHNpcF1.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.CiJjK6WX.mjs').then(interopDefault),
   "pages/index.vue": () => import('./index-styles.Cgzmi_6u.mjs').then(interopDefault),
   "components/HeroSection.vue": () => import('./HeroSection-styles.7CfN4Ghs.mjs').then(interopDefault),
   "components/GridMaker.vue": () => import('./GridMaker-styles.7ykR8MC-.mjs').then(interopDefault),
   "pages/about.vue": () => import('./about-styles.2Sgea3YL.mjs').then(interopDefault),
-  "pages/terms.vue": () => import('./terms-styles.YH8zH6It.mjs').then(interopDefault),
-  "pages/privacy.vue": () => import('./privacy-styles.DLQSIHDa.mjs').then(interopDefault),
   "pages/pixel-art.vue": () => import('./pixel-art-styles.BcrW8nzX.mjs').then(interopDefault),
+  "pages/privacy.vue": () => import('./privacy-styles.DLQSIHDa.mjs').then(interopDefault),
+  "pages/terms.vue": () => import('./terms-styles.YH8zH6It.mjs').then(interopDefault),
   "pages/edit-grid.vue": () => import('./edit-grid-styles.CAZjlOz5.mjs').then(interopDefault),
   "pages/advanced.vue": () => import('./advanced-styles.C0he_Lf8.mjs').then(interopDefault),
-  "pages/portrait-drawing-grid.vue": () => import('./portrait-drawing-grid-styles.F_c5inoU.mjs').then(interopDefault),
   "pages/perler-bead.vue": () => import('./perler-bead-styles.Bv52wi-G.mjs').then(interopDefault),
+  "pages/portrait-drawing-grid.vue": () => import('./portrait-drawing-grid-styles.F_c5inoU.mjs').then(interopDefault),
   "components/AppNavbar.vue": () => import('./AppNavbar-styles.CvedNDgV.mjs').then(interopDefault),
   "components/ExternalLinks.vue": () => import('./ExternalLinks-styles.DShRNlTv.mjs').then(interopDefault),
   "components/ActionSection.vue": () => import('./ActionSection-styles.BSWfRLr6.mjs').then(interopDefault),
   "components/FeaturesSection.vue": () => import('./FeaturesSection-styles.QRDexVyX.mjs').then(interopDefault),
   "components/FeatureSection.vue": () => import('./FeatureSection-styles.BdZkdw4_.mjs').then(interopDefault),
-  "components/HowToGuide.vue": () => import('./HowToGuide-styles.CtzU_4T1.mjs').then(interopDefault),
-  "components/PageGuide.vue": () => import('./PageGuide-styles.BHOSVF9Z.mjs').then(interopDefault)
+  "components/PageGuide.vue": () => import('./PageGuide-styles.BHOSVF9Z.mjs').then(interopDefault),
+  "components/HowToGuide.vue": () => import('./HowToGuide-styles.CtzU_4T1.mjs').then(interopDefault)
 };
 
 export { styles as default };
